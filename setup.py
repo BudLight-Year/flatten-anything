635158877412cf1408757ec9de0953c9bc31b065
BudLight-Year <totallysweethobo@gmail.com>
2025-09-24 15:54:55 -0600

    updated version on setup and init.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="flatten-anything",
-    version="1.1.0",
+    version="1.1.1",
     author="Evan Cline",
     author_email="totallysweethobo@gmail.com",
     description="Stop writing custom parsers for every data format. Flatten anything.",
